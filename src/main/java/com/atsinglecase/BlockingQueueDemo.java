892d70407f120374d628d0e9d9306e650365bad3
vdal <731217140@qq.com>
2020-12-25 16:35:29 +0800

    并发例子

--- src/main/java/com/atsinglecase/BlockingQueueDemo.java
+++ src/main/java/com/atsinglecase/BlockingQueueDemo.java
@@ -1,12 +1,13 @@
 package com.atsinglecase;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.SynchronousQueue;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.concurrent.*;
 /*ArrayBlockingQueue 是一个基于数组结构的有界阻塞队列，此队列按FIFO（先进先出） 原则对元素进行排序
 * LinkedBlockingDeque 一个基于链表结构的阻塞队列，此队列按FIFO（先进先出）排序元素 吞吐量通常高于ArrayBlockingQueue
 * SynchronousQueue 一个不存储元素的阻塞队列，每个插入操作必须等到另一个线程调用移除操作，否则插入操作一直处于阻塞状态，吞吐量通常高于ArrayBlockingQueue
-*
+*线程池的底层就这三个
 * 1.队列
 * 2.阻塞队列   当阻塞队列是空时，从队列中获取元素的操作会被阻塞 当阻塞队列时满时，往队列里添加元素的操作会被阻塞
 *   2.1阻塞队列没有好的一面
@@ -15,6 +16,18 @@ import java.util.concurrent.SynchronousQueue;
 
 public class BlockingQueueDemo {
     public static void main(String[] args) {
+        BlockingQueue<String> blockingQueue=new ArrayBlockingQueue<>(3);
+
+        System.out.println(blockingQueue.add("a"));
+        System.out.println(blockingQueue.add("b"));
+        System.out.println(blockingQueue.add("c"));
+
+
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+/*        HashMap hashMap=new HashMap();
+        hashMap.put()*/
 
     }
 }
---
+++ src/main/java/com/atsinglecase/ProdConsumer_TraditionDemo.java
@@ -0,0 +1,4 @@
+package com.atsinglecase;
+/*一个初始值为零的变量 两个线程对其交替操作*/
+public class ProdConsumer_TraditionDemo {
+}
